abfd5ba537498995b23edc1881e3d948553defa9
Cristhian G. <kris.gar.jim@gmail.com>
2025-03-28 12:30:52 -0500

    commit 28/03/25

--- Reto3/Proyecto_MVC_Fruver/main.py
+++ Reto3/Proyecto_MVC_Fruver/main.py
@@ -4,12 +4,16 @@ from View.view import ViewShopping
 model = Shopping()
 
 # Agregado de productos de ejemplo
-model.add_product(1, 'Manzana', 1.5, 100)
-model.add_product(2, 'Pera', 2, 100)
-model.add_product(3, 'Banana', 1, 100)
-model.add_product(4, 'Naranja', 1.5, 100)
-model.add_product(5, 'Uva', 3, 100)
-model.add_product(6, 'Sandía', 5, 100)  
+model.add_product(1, 'Manzana', 1500, 100)
+model.add_product(2, 'Pera', 1000, 100)
+model.add_product(3, 'Banana', 400, 100)
+model.add_product(4, 'Naranja', 500, 100)
+model.add_product(5, 'Uva', 200, 100)
+model.add_product(6, 'Sandía', 2500, 100)  
+model.add_product(7, 'Mango', 1800, 100)  
+model.add_product(8, 'Maracuya', 900, 100)  
+model.add_product(9, 'Limon', 300, 100)  
+model.add_product(10, 'Melon', 1700, 100)  
 
 controller = ControllerShopping(model, None)  
 view = ViewShopping(controller)
